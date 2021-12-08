f84ffc05f1a8fb35df7c03aa1edf897bfcbe56e3
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-12-08 09:39:53 +0100

    Initial Commit

---
+++ Mobilev2Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobilev2Test
+//
+//  Created by Søren Nielsen on 08/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
